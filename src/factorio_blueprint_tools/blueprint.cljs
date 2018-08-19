4613a15c0f2c21ffe7506434b826040cee3bd5a8
Christoph Frick <cf@ofnir.net>
2018-08-19 19:11:02 +0200

    Move possibly useful functions out of tile into a common blueprint ns

---
+++ src/factorio_blueprint_tools/blueprint.cljs
@@ -0,0 +1,30 @@
+(ns factorio-blueprint-tools.blueprint
+  (:require [com.rpl.specter :as s]
+            [factorio-blueprint-tools.sizes :as sizes]
+            [factorio-blueprint-tools.coord :as coord]))
+
+(defn fix-entity-numbers
+  [entities]
+  (mapv (fn [entity number]
+          (assoc entity :entity_number number))
+        entities
+        (iterate inc 1)))
+
+(defn entity-area
+  [{:keys [name position direction] :as entity}]
+  (-> (sizes/selection-box name)
+      (coord/rotate-box direction)
+      (coord/translate-box (coord/coord (:x position) (:y position)))))
+
+(defn entities-area
+  [blueprint]
+  (transduce
+   (map entity-area)
+   (completing coord/union-box)
+   (s/select [:blueprint :entities s/ALL (s/submap [:name :position :direction])] blueprint)))
+
+(defn move-position
+  [position x-offset y-offset]
+  (s/multi-transform [:position (s/multi-path [:x (s/terminal #(+ % x-offset))]
+                                              [:y (s/terminal #(+ % y-offset))])]
+                     position))
