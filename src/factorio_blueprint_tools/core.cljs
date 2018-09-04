9c2560734fd185ff586e32aab81f2f5d8094cfb6
Christoph Frick <cf@ofnir.net>
2018-09-04 22:40:48 +0200

    Rework `upgrade` to use citrus; final code arrangements and cleanup

--- src/factorio_blueprint_tools/core.cljs
+++ src/factorio_blueprint_tools/core.cljs
@@ -11,103 +11,21 @@
 
 (enable-console-print!)
 
+;;; Components
+
+; Tools
+
 (def ta-no-spellcheck
   {:autoComplete "off"
    :autoCorrect "off"
    :autoCapitalize "off"
    :spellCheck "false"})
 
-(def blueprint-state
-  {::blueprint-error nil ; maybe an error
-   ::blueprint nil ; the blueprint, unless there is an error
-   })
-
-(defn decode-blueprint
-  [encoded-blueprint]
-  (if (or (not encoded-blueprint) (str/blank? encoded-blueprint))
-    [nil nil]
-    (try
-      [(ser/decode encoded-blueprint) nil]
-      (catch :default e
-        [nil e]))))
-
-(defn build-blueprint-watch
-  [watch-name blueprint-string-atom blueprint-target-atom]
-  (add-watch blueprint-string-atom watch-name
-             (fn [_ _ _ blueprint-string]
-               (letfn [(update-fn
-                         [blueprint error]
-                         (swap! blueprint-target-atom assoc
-                                ::blueprint blueprint
-                                ::blueprint-error error))]
-                 (if (seq blueprint-string)
-                   (try
-                     (update-fn (ser/decode blueprint-string) nil)
-                     (catch :default e
-                       (update-fn nil (str "Could not load blueprint.  Please make sure to copy and paste the whole string from Factorio. (Error: " e ")"))))
-                   (update-fn nil nil))))))
-
-(defn blueprint-state-cursors
-  "Create rum/cursors for ::blueprint, ::blueprint-error, and each optional key passed and returns as vector in that order"
-  [state & ks]
-  (let [ks (concat [::blueprint ::blueprint-error] ks)]
-    (mapv #(rum/cursor state %) ks)))
-
-(defn form-item-input-blueprint
-  [state]
-  (ant/form-item {:label "Blueprint string"
-                  :help "Copy a blueprint string from Factorio and paste it in this field"}
-                 (ant/input-text-area (assoc ta-no-spellcheck
-                                             :value (rum/react state)
-                                             :onChange #(reset! state (-> % .-target .-value))
-                                             :onFocus #(.select (-> % .-target))))))
-
-(defn form-item-output-blueprint
-  [state]
-  (ant/form-item {:label "Result"
-                  :help "Copy this blueprint string and import in from the blueprint library in Factorio"}
-                 (ant/input-text-area (assoc ta-no-spellcheck
-                                             :value (rum/react state)
-                                             :onFocus #(.select (-> % .-target))))))
-
 (defn alert-error
   [error-message]
   (ant/alert {:message error-message
               :showIcon true
               :type "error"}))
-
-;;; About
-
-(rum/defc ContentAbout < rum/static
-  []
-  (ant/layout-content
-   {:style {:padding "1ex 1em"}}
-   [:h2 "Random tools to manipulate Factorio blueprint strings"]
-   [:p "While there are already some of those functions built as mods to the game, one can not use mods while playing for the achievements"]
-   [:h3 "Instructions"]
-   [:p "Pick a tool on the left hand side in the menu:"]
-   [:ul
-    [:li [:em "Tile"] ": Arrange copies of the blueprint in a grid.  E.g. take a six electric miner blueprint and tile 15x15 to cover even the biggest resource fields"]
-    [:li [:em "Mirror"] ": Mirror the blueprint either vertically or horizontally"]
-    [:li [:em "Upgrade"] ": Decide what common upgradeable entities (e.g. inserters) to upgrade.  Also supports downgrading (e.g. you have a great blueprint but not the tech yet)"]]
-   [:p "Then paste the blueprint string either from the game or from a different place into the input field, adjust the settings, and finally copy the final blueprint and import it into Factorio"]))
-
-;;; Settings 
-
-(rum/defc ContentSettings < rum/static
-  []
-  (ant/layout-content
-   {:style {:padding "1ex 1em"}}
-   [:h2 "Settings"]
-   (ant/alert {:message "Currently there is no way to change or add mods etc. for the sizes occupied by the entities."
-               :showIcon true
-               :type "warning"})
-   (ant/form
-    (ant/form-item {:label "Factorio entities"}
-                   (ant/select {:value "vanilla-0.16"}
-                               (ant/select-option {:key "vanilla-0.16"} "Vanilla 0.16"))))))
-
-;;; Tools
 
 (rum/defc BlueprintInput <
   rum/reactive
@@ -138,7 +56,38 @@
                   (when-let [blueprint (rum/react (citrus/subscription r [controller :output :blueprint]))]
                     (preview/preview blueprint))]))
 
-;;; Tile
+; About
+
+(rum/defc ContentAbout < rum/static
+  []
+  (ant/layout-content
+   {:style {:padding "1ex 1em"}}
+   [:h2 "Random tools to manipulate Factorio blueprint strings"]
+   [:p "While there are already some of those functions built as mods to the game, one can not use mods while playing for the achievements"]
+   [:h3 "Instructions"]
+   [:p "Pick a tool on the left hand side in the menu:"]
+   [:ul
+    [:li [:em "Tile"] ": Arrange copies of the blueprint in a grid.  E.g. take a six electric miner blueprint and tile 15x15 to cover even the biggest resource fields"]
+    [:li [:em "Mirror"] ": Mirror the blueprint either vertically or horizontally"]
+    [:li [:em "Upgrade"] ": Decide what common upgradeable entities (e.g. inserters) to upgrade.  Also supports downgrading (e.g. you have a great blueprint but not the tech yet)"]]
+   [:p "Then paste the blueprint string either from the game or from a different place into the input field, adjust the settings, and finally copy the final blueprint and import it into Factorio"]))
+
+; Settings 
+
+(rum/defc ContentSettings < rum/static
+  []
+  (ant/layout-content
+   {:style {:padding "1ex 1em"}}
+   [:h2 "Settings"]
+   (ant/alert {:message "Currently there is no way to change or add mods etc. for the sizes occupied by the entities."
+               :showIcon true
+               :type "warning"})
+   (ant/form
+    (ant/form-item {:label "Factorio entities"}
+                   (ant/select {:value "vanilla-0.16"}
+                               (ant/select-option {:key "vanilla-0.16"} "Vanilla 0.16"))))))
+
+; Tile
 
 (rum/defc ContentTile <
   rum/reactive
@@ -161,7 +110,7 @@
                                          :min 1}))
        (BlueprintOutput r :tile))])))
 
-;;; Mirror
+; Mirror
 
 (rum/defc ContentMirror <
   rum/reactive
@@ -182,52 +131,28 @@
 
        (BlueprintOutput r :mirror))])))
 
-;;; Upgrade
+; Upgrade
 
-(defonce blueprint-upgrade-state
-  (atom ""))
-
-(defonce upgrade-settings-state
-  (atom
-   (assoc blueprint-state
-          ::upgrade-config upgrade/default-upgrade-config)))
-
-(defonce update-blueprint-upgrade-watch
-  (build-blueprint-watch ::update-blueprint-upgrade blueprint-upgrade-state upgrade-settings-state))
-
-(defonce upgrade-result-state
-  (rum/derived-atom [upgrade-settings-state] ::upgrade-result
-                    (fn [{::keys [blueprint upgrade-config] :as upgrade-settings}]
-                      (some->> blueprint (upgrade/upgrade-blueprint upgrade-config)))))
-
-(defonce upgrade-result-serialized-state
-  (rum/derived-atom [upgrade-result-state] ::upgrade-result-serialized #(some-> % ser/encode)))
-
-(rum/defcs ContentUpgrade <
+(rum/defc ContentUpgrade <
   rum/reactive
-  []
-  (let [[blueprint blueprint-error upgrade-config] (blueprint-state-cursors upgrade-settings-state ::upgrade-config)]
-    (ant/layout-content
-     {:style {:padding "1ex 1em"}}
-     [:h2 "Upgrade (or downgrade) a blueprint"]
-     (ant/form
-      (form-item-input-blueprint blueprint-upgrade-state)
-      (when-let [error-message (rum/react blueprint-error)]
-        (alert-error error-message)))
-     (when-let [blueprint (rum/react blueprint)]
-       [:div
-        (let [upgradable (upgrade/upgradeable-from-blueprint blueprint)
-              order (filter upgradable upgrade/upgrades-order)
-              cfg (rum/react upgrade-config)]
-          (ant/form
-           (for [from order]
-             (ant/form-item {:label (upgrade/upgrades-names from)}
-                            (ant/radio-group {:value (cfg from)
-                                              :onChange #(swap! upgrade-config assoc from (-> % .-target .-value))}
-                                             (for [option (upgrade/upgrades-by-key from)]
-                                               (ant/radio {:key option :value option} (upgrade/upgrades-names option))))))
-           (form-item-output-blueprint upgrade-result-serialized-state)))
-        (preview/preview (rum/react upgrade-result-state))]))))
+  [r]
+  (ant/layout-content
+   {:style {:padding "1ex 1em"}}
+   [:h2 "Upgrade (or downgrade) a blueprint"]
+   (ant/form
+    (BlueprintInput r :upgrade))
+   (when-let [blueprint (rum/react (citrus/subscription r [:upgrade :input :blueprint]))]
+     [:div
+      (let [upgradable (upgrade/upgradeable-from-blueprint blueprint)
+            order (filter upgradable upgrade/upgrades-order)]
+        (ant/form
+         (for [from order]
+           (ant/form-item {:label (upgrade/upgrades-names from)}
+                          (ant/radio-group {:value (rum/react (citrus/subscription r [:upgrade :config from]))
+                                            :onChange #(citrus/dispatch! r :upgrade :set-config from (-> % .-target .-value))}
+                                           (for [option (upgrade/upgrades-by-key from)]
+                                             (ant/radio {:key option :value option} (upgrade/upgrades-names option))))))
+         (BlueprintOutput r :upgrade)))])))
 
 ;;; Main
 
@@ -241,7 +166,7 @@
 (def navigations-by-key
   (into {} (map (juxt :key identity)) navigations))
 
-;;; Controller (might end up in a differnt file
+;; Controller (might end up in a differnt file
 
 ; Navigation
 
@@ -253,7 +178,7 @@
 (defmethod navigation :goto [_ [target] _]
   {:state target})
 
-;
+; Helpers
 
 (def default-tool-state
   {:input {:encoded nil
@@ -262,6 +187,15 @@
    :config {}
    :output {:encoded nil
             :blueprint nil}})
+
+(defn decode-blueprint
+  [encoded-blueprint]
+  (if (or (not encoded-blueprint) (str/blank? encoded-blueprint))
+    [nil nil]
+    (try
+      [(ser/decode encoded-blueprint) nil]
+      (catch :default e
+        [nil e]))))
 
 (defn- set-blueprint
   [state encoded-blueprint]
@@ -280,6 +214,8 @@
         encoded-result (some-> result ser/encode)]
     (update state :output assoc :blueprint result :encoded encoded-result)))
 
+
+; Tile
 
 (def default-tile-config
   {:tile-x 2
@@ -305,6 +241,8 @@
                          (fn tile [blueprint {:keys [tile-x tile-y] :as config}]
                            (tile/tile blueprint tile-x tile-y)))})
 
+; Mirror
+
 (def default-mirror-config
   {:direction :vertically})
 
@@ -314,11 +252,11 @@
   {:state (assoc default-tool-state
                  :config default-mirror-config)})
 
-(defmethod mirror :set-blueprint [r [encoded-blueprint] state]
+(defmethod mirror :set-blueprint [_ [encoded-blueprint] state]
   {:state (set-blueprint state encoded-blueprint)
    :dispatch [[:mirror :update]]})
 
-(defmethod mirror :set-config [r [k v] state]
+(defmethod mirror :set-config [_ [k v] state]
   {:state (set-config state k v)
    :dispatch [[:mirror :update]]})
 
@@ -329,16 +267,45 @@
                            (mirror/mirror blueprint direction)))})
 
 
+; Upgrade
+
+(defmulti upgrade identity)
+
+(defmethod upgrade :init []
+  {:state (assoc default-tool-state
+                 :config upgrade/default-upgrade-config)})
+
+(defmethod upgrade :set-blueprint [_ [encoded-blueprint] state]
+  {:state (set-blueprint state encoded-blueprint)
+   :dispatch [[:upgrade :update]]})
+
+(defmethod upgrade :set-config [_ [k v] state]
+  {:state (set-config state k v)
+   :dispatch [[:upgrade :update]]})
+
+(defmethod upgrade :update [_ _ state]
+  {:state (update-result state
+                         upgrade/default-upgrade-config
+                         (fn upgrade [blueprint config]
+                           (upgrade/upgrade-blueprint config blueprint)))})
+
+
+;; Effect Handlers
+
 (defn dispatch [r _ events]
   (doseq [[ctrl & args] events]
     (apply citrus/dispatch! (into [r ctrl] args))))
+
+
+;; Reconciler
 
 (defonce reconciler
   (citrus/reconciler
    {:state (atom {})
     :controllers {:navigation navigation
                   :tile tile
-                  :mirror mirror}
+                  :mirror mirror
+                  :upgrade upgrade}
     :effect-handlers {:dispatch dispatch}}))
 
 ;;; Main content
