94e8b42bf85a3fcb920a3e571887419efae0e7ee
Christoph Frick <cf@ofnir.net>
2018-09-10 09:48:24 +0200

    Add helper to add map entries for a bidirectional mapping

--- src/factorio_blueprint_tools/mirror.cljs
+++ src/factorio_blueprint_tools/mirror.cljs
@@ -2,14 +2,18 @@
   (:require [com.rpl.specter :as s]
             [factorio-blueprint-tools.blueprint :as blueprint]))
 
+(defn- add-inverse
+  [m]
+  (into m (for [[k v] m] [v k])))
+
 ; curved rails are tricky with their direction:
 ; a full circle looks like this (starting with at 01:30 o'clock):
 ; 3 0 5 2 7 4 1 6 
 
 (def directions
   ; direction, axis to negate, direction to change; direction to change for curved rails
-  {:vertically [:x {0 0, 1 7, 2 6, 3 5, 4 4, 5 3, 6 2, 7 1} {0 1, 1 0, 2 7, 3 6, 4 5, 5 4, 6 3, 7 2}]
-   :horizontally [:y {0 4, 1 3, 2 2, 3 1, 4 0, 5 7, 6 6, 7 5} {0 5, 1 4, 2 3, 3 2, 4 1, 5 0, 6 7, 7 6}]})
+  {:vertically [:x {0 0, 1 7, 2 6, 3 5, 4 4, 5 3, 6 2, 7 1} (add-inverse {0 1, 2 7, 3 6, 4 5})]
+   :horizontally [:y {0 4, 1 3, 2 2, 3 1, 4 0, 5 7, 6 6, 7 5} (add-inverse {0 5, 1 4, 2 3, 6 7})]})
 
 (defn- mirror-direction
   [mapping direction]
