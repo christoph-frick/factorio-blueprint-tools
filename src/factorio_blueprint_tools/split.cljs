03fe1e812281a87634f358bbdc91f0f852ce9c16
Christoph Frick <cf@ofnir.net>
2019-07-22 22:24:48 +0200

    Center splits on [0,0]
    Fixes: #6

--- src/factorio_blueprint_tools/split.cljs
+++ src/factorio_blueprint_tools/split.cljs
@@ -12,6 +12,7 @@
   (when-let [area (blueprint/entities-area blueprint)]
     (let  [label (or (some-> blueprint :blueprint :label) "Anonymous blueprint")
            [[min-x min-y] [max-x max-y]] area
+           tile-size-half (Math/floor (/ tile-size 2))
            width (Math/ceil (- max-x min-x))
            height (Math/ceil (- max-y min-y))
            x-tiles (Math/ceil (/ width tile-size))
@@ -20,13 +21,14 @@
                             x (range x-tiles)]
                         (let [filter-box (-> (coord/box-from-size coord/ZERO tile-size tile-size)
                                              (coord/translate-box (coord/coord (* x tile-size) (* y tile-size)))
-                                             (coord/translate-box (coord/coord min-x min-y)))]
+                                             (coord/translate-box (coord/coord min-x min-y)))
+                              [[offset-x offset-y] _] filter-box]
                           (-> blueprint
                               (assoc-in [:blueprint :label] (str label " [" x "|" y "]"))
                               (update-in [:blueprint :entities]
                                          (fn [entities]
                                            (->> entities
                                                 (filter (partial entity-in-box? filter-box))
-                                                (map #(blueprint/move-position % (- (* x tile-size)) (- (* y tile-size))))
+                                                (map #(blueprint/move-position % (- (- tile-size-half) offset-x) (- (- tile-size-half) offset-y)))
                                                 (blueprint/fix-entity-numbers)))))))]
       (blueprint/book (str label " " x-tiles " x " y-tiles) blueprints))))
