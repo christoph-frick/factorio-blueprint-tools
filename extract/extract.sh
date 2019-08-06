157c62dd4f589d83ef21ca5bad7ccbb7e4f0c013
Christoph Frick <cf@ofnir.net>
2019-08-06 16:08:43 +0200

    Rebuild extraction tool on extract

--- extract/extract.sh
+++ extract/extract.sh
@@ -10,4 +10,5 @@ cd factorio-data
 git pull
 cd ..
 
+docker-compose build --force-rm --pull
 docker-compose run --rm -u `id -u`:`id -g` -v `realpath factorio-data`:/var/factorio-data -v `realpath ../resources`:/var/extract-result extract
